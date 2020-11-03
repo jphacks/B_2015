80eacc0a3264e4890dcbfae092dd9c8e2272e8a6
fujimotoayaka <fujimotoayaka.happy@gmail.com>
2020-11-03 18:53:51 +0900

    main.pyの追加

---
+++ main.py
@@ -0,0 +1,36 @@
+param = input()
+
+def main(param):
+    from janome.tokenizer import Tokenizer
+    t = Tokenizer()
+    #print(param2)
+    def search(param):
+        param2 = '"' + param + '":'
+        gokan_sentence_list = []
+        with open('gokan_dict.tsv', encoding = 'utf-8')as f:
+            for line in f:
+                #print(line)
+                if param2 in line:
+                    _,line = line.split(param2)
+                    gokan_sentence_list = line.split('\t')
+                    break;
+        if gokan_sentence_list == []:
+            return []
+        return gokan_sentence_list
+
+
+    gokan_sentence_list = search(param)
+    #print(gokan_dict)          
+    from dictionary import make_synonym_dict
+    synonym_dict = {}
+    synonym_dict=make_synonym_dict(param)
+    print(synonym_dict)
+    for synonym in synonym_dict[param]:
+            print(synonym)
+            gokan_sentence_list += search(synonym)
+            #if gokan_dict.get(synonym):
+    for sentence in gokan_sentence_list:
+                    print(sentence)
+            
+            
+main(param)
