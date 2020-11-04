d04d0242123b76b58058879c3ea4685635bc7ecd
si-tm <kumomaya@gmail.com>
2020-11-04 23:23:45 +0000

    requests installed

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5!ka2$_jfqm@_5n+kwcpo8*@ddj#qi(qhv%*-d6bu*9p#q&j!k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [sitm.pythonanywhere.com]
+ALLOWED_HOSTS = ['sitm.pythonanywhere.com']
 
 
 # Application definition
