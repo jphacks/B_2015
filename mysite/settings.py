454efc3e8cb1b1c9c18b78740178063523a5bdf4
si-tm <kumomaya@gmail.com>
2020-11-07 11:28:12 +0900

    setting ver2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = '5!ka2$_jfqm@_5n+kwcpo8*@ddj#qi(qhv%*-d6bu*9p#q&j!k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['sitm.pythonanywhere.com',
+                    '127.0.0.1',
+                    ]
+
 
 
 # Application definition
